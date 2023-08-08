4ccbcec3ad147229f994cee30edf55d1e24faea0
Cyborg 9 <sarthaksns@gmail.com>
2023-08-08 19:16:20 +0530

    made a simulation of game of life (fully working)

---
+++ src/app.js
@@ -0,0 +1,103 @@
+const playground = document.getElementById("playground");
+const num = 100;
+let timerId = null;
+let cur_gen = initialzing(num, playground);
+
+// to start the simulation
+
+document.getElementById("start").addEventListener("click", () => {
+  console.log("game started");
+  timerId = setInterval(gameOfLife, 250);
+});
+
+// to stop the simulation
+document.getElementById("stop").addEventListener("click", () => {
+  clearInterval(timerId);
+  console.log("game stopped");
+});
+
+console.log("hello world ");
+
+function initialzing(num, playground) {
+  console.log("inside initilize");
+  playground.innerHTML = null;
+  const arr = [];
+  for (let i = 0; i < num; i++) {
+    const ar = [];
+    // creating rows to store the cell in each row
+    const row = document.createElement("div");
+    row.setAttribute("class", "rows");
+    for (let j = 0; j < num; j++) {
+      ar.push(false);
+      // adding cell to rows
+      const cell = document.createElement("div");
+      cell.setAttribute("class", "cell");
+      const id = String(i) + "," + String(j);
+      cell.setAttribute("id", id);
+      row.appendChild(cell);
+    }
+    arr.push(ar);
+    playground.appendChild(row);
+  }
+  return arr;
+}
+
+playground.addEventListener("click", (e) => {
+  //console.log(e.target.id);
+  if (e.target.hasChildNodes()) return;
+
+  let index = e.target.id.split(",");
+
+  if (cur_gen[Number(index[0])][Number(index[1])]) {
+    e.target.setAttribute("class", "cell");
+    cur_gen[Number(index[0])][Number(index[1])] = false;
+  } else {
+    e.target.setAttribute("class", "cellLive");
+    cur_gen[Number(index[0])][Number(index[1])] = true;
+  }
+});
+
+//// game of life function
+const gameOfLife = () => {
+  console.log("inside game of life ");
+  const arr = [];
+  for (let i = 0; i < num; i++) {
+    const ar = [];
+    for (let j = 0; j < num; j++) ar.push(false);
+
+    arr.push(ar);
+  }
+  const next_gen = arr;
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      let neighbour = 0;
+      //upp
+      if (i > 0 && cur_gen[i - 1][j] === true) neighbour++;
+
+      // left
+      if (j > 0 && cur_gen[i][j - 1] === true) neighbour++;
+
+      // down
+      if (i < num - 1 && cur_gen[i + 1][j] === true) neighbour++;
+
+      //right
+      if (j < num - 1 && cur_gen[i][j + 1] === true) neighbour++;
+
+      //check for the current life or death of a cell
+      if (neighbour < 2 || neighbour > 3) {
+        next_gen[i][j] = false;
+        document
+          .getElementById(String(i) + "," + String(j))
+          .setAttribute("class", "cell");
+      } else if (neighbour <= 3) {
+        next_gen[i][j] = true;
+        document
+          .getElementById(String(i) + "," + String(j))
+          .setAttribute("class", "cellLive");
+      }
+    }
+  }
+  cur_gen = next_gen;
+
+  return;
+};
